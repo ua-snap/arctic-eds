7067cdd3029b99a8531c9d4cc3121e8a3109d5fb
Bruce Crevensten <bruce.crevensten@gmail.com>
2021-10-09 13:28:11 -0800

    Add physiography plate

---
+++ components/plates/physiography/layers.js
@@ -0,0 +1,9 @@
+export default [
+	{
+		id: "ak_level3_ecoregions",
+		title: "Alaska Level 3 Ecoregions",
+		wmsLayerName:
+			"physiography:ak_level3_ecoregions",
+		default: true
+	}
+];
---
+++ pages/physiography/physiography.vue
@@ -0,0 +1,33 @@
+<template>
+	<div>
+		<div class="container">
+			<section class="section">
+				<h1 class="title is-2">Physiographic Provinces</h1>
+				<div class="content content-clamp is-size-4">
+					<p>
+						Ecoregions are essentially equivalent to the more archaic "physiographic provinces." Ecoregions do combine landscape and vegetation variables, but have less detail about vegetation when compared to "land cover" or "forest type" datasets. Topography plays a prominent role.
+					</p>
+				</div>
+				<div class="content content-clamp is-size-5">
+					<p>
+						Click any point on the map, or enter a lat/lon, to get the geological unit for that point.
+					</p>
+				</div>
+			</section>
+		</div>
+		<PhysiographyController />
+	</div>
+</template>
+<script lang="scss" scoped></script>
+<script>
+
+import PhysiographyController from "~/components/plates/physiography/Controller";
+
+export default {
+	name: "Physiography",
+	layout: "plate",
+	components: {
+		PhysiographyController
+	}
+};
+</script>
