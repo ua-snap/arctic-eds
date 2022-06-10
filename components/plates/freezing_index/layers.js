14b815eae2c2785eefa15c8b234d256e41b0ee6a
Bruce Crevensten <bruce.crevensten@gmail.com>
2022-06-10 15:23:53 -0700

    Remove single-slice layers from freezing index, temp plates

--- components/plates/freezing_index/layers.js
+++ components/plates/freezing_index/layers.js
@@ -14,37 +14,4 @@ export default [
     wmsLayerName: "freezing_index",
     style: "arctic_eds_freezing_index_future_condensed",
   },
-  {
-    id: "ncarccsm4_freezing_index_2040",
-    title: "Projected (2040, NCAR CCSM4, RCP 8.5)",
-    source: "rasdaman",
-    wmsLayerName: "freezing_index",
-    rasdamanConfiguration: {
-      dim_model: 2,
-      time: "2040-01-01T00:00:00.000Z",
-    },
-    style: "arctic_eds",
-  },
-  {
-    id: "ncarccsm4_freezing_index",
-    title: "Projected (2100, NCAR CCSM4, RCP 8.5)",
-    source: "rasdaman",
-    wmsLayerName: "freezing_index",
-    rasdamanConfiguration: {
-      dim_model: 2,
-      time: "2100-01-01T00:00:00.000Z",
-    },
-    style: "arctic_eds",
-  },
-  {
-    id: "historical_freezing_index",
-    title: "Historical (1979)",
-    source: "rasdaman",
-    wmsLayerName: "freezing_index",
-    rasdamanConfiguration: {
-      dim_model: 0,
-      time: "1979-01-01T00:00:00.000Z",
-    },
-    style: "arctic_eds",
-  },
 ];
