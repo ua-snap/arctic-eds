c769e6c3f4d8c739d2f25a5c6b5842a3551b916e
Craig Stephenson <crstephenson@alaska.edu>
2022-06-15 16:57:17 -0800

    Add conversion widget to permafrost report + support for meters/inches conversion.

--- components/plates/permafrost/Report.vue
+++ components/plates/permafrost/Report.vue
@@ -9,6 +9,13 @@
 
       <MiniMap />
 
+      <UnitRadio class="my-5"
+        :patterns="[
+          { type: 'temperature', substring: 'magt_' },
+          { type: 'm_in', substring: 'alt_' }
+        ]"
+      />
+
       <h4 class="title is-4">
         GIPL Mean Annual Ground Temperature (<UnitWidget />)
       </h4>
@@ -24,42 +31,42 @@
         <tbody>
           <tr>
             <th scope="row">Historical (1995)</th>
-            <td>{{ results.gipl_magt_1995 }} </td>
+            <td>{{ results.gipl_magt_1995 }}<UnitWidget type="light" /> </td>
             <td></td>
           </tr>
           <tr>
             <th scope="row">2011 - 2040</th>
-            <td>{{ results.gipl_magt_2025 }}</td>
+            <td>{{ results.gipl_magt_2025 }}<UnitWidget type="light" /></td>
             <td>
-              {{ signedDiff(results.gipl_magt_1995, results.gipl_magt_2025) }}
+              {{ signedDiff(results.gipl_magt_1995, results.gipl_magt_2025, 'magt') }}<UnitWidget type="light" />
             </td>
           </tr>
           <tr>
             <th scope="row">2036 - 2065</th>
-            <td>{{ results.gipl_magt_2050 }}</td>
+            <td>{{ results.gipl_magt_2050 }}<UnitWidget type="light" /></td>
             <td>
-              {{ signedDiff(results.gipl_magt_1995, results.gipl_magt_2050) }}
+              {{ signedDiff(results.gipl_magt_1995, results.gipl_magt_2050, 'magt') }}<UnitWidget type="light" />
             </td>
           </tr>
           <tr>
             <th scope="row">2061 – 2090</th>
-            <td>{{ results.gipl_magt_2075 }}</td>
+            <td>{{ results.gipl_magt_2075 }}<UnitWidget type="light" /></td>
             <td>
-              {{ signedDiff(results.gipl_magt_1995, results.gipl_magt_2075) }}
+              {{ signedDiff(results.gipl_magt_1995, results.gipl_magt_2075, 'magt') }}<UnitWidget type="light" />
             </td>
           </tr>
           <tr>
             <th scope="row">2086 – 2100</th>
-            <td>{{ results.gipl_magt_2095 }}</td>
+            <td>{{ results.gipl_magt_2095 }}<UnitWidget type="light" /></td>
             <td>
-              {{ signedDiff(results.gipl_magt_1995, results.gipl_magt_2095) }}
+              {{ signedDiff(results.gipl_magt_1995, results.gipl_magt_2095, 'magt') }}<UnitWidget type="light" />
             </td>
           </tr>
         </tbody>
       </table>
 
       <div v-if="results.gipl_alt_1995">
-        <h4 class="title is-4">GIPL Active Layer Thickness (m)</h4>
+        <h4 class="title is-4">GIPL Active Layer Thickness (<UnitWidget unitType="m_in" />)</h4>
         <table class="table">
           <thead>
             <tr>
@@ -71,35 +78,35 @@
           <tbody>
             <tr>
               <th scope="row">Historical (1995)</th>
-              <td>{{ results.gipl_alt_1995 }}</td>
+              <td>{{ results.gipl_alt_1995 }}<UnitWidget unitType="m_in" type="light" /></td>
               <td></td>
             </tr>
             <tr>
               <th scope="row">2011 - 2040</th>
-              <td>{{ results.gipl_alt_2025 }}</td>
+              <td>{{ results.gipl_alt_2025 }}<UnitWidget unitType="m_in" type="light" /></td>
               <td>
-                {{ signedDiff(results.gipl_alt_1995, results.gipl_alt_2025) }}
+                {{ signedDiff(results.gipl_alt_1995, results.gipl_alt_2025, 'alt') }}<UnitWidget unitType="m_in" type="light" />
               </td>
             </tr>
             <tr>
               <th scope="row">2036 - 2065</th>
-              <td>{{ results.gipl_alt_2050 }}</td>
+              <td>{{ results.gipl_alt_2050 }}<UnitWidget unitType="m_in" type="light" /></td>
               <td>
-                {{ signedDiff(results.gipl_alt_1995, results.gipl_alt_2050) }}
+                {{ signedDiff(results.gipl_alt_1995, results.gipl_alt_2050, 'alt') }}<UnitWidget unitType="m_in" type="light" />
               </td>
             </tr>
             <tr>
               <th scope="row">2061 – 2090</th>
-              <td>{{ results.gipl_alt_2075 }}</td>
+              <td>{{ results.gipl_alt_2075 }}<UnitWidget unitType="m_in" type="light" /></td>
               <td>
-                {{ signedDiff(results.gipl_alt_1995, results.gipl_alt_2075) }}
+                {{ signedDiff(results.gipl_alt_1995, results.gipl_alt_2075, 'alt') }}<UnitWidget unitType="m_in" type="light" />
               </td>
             </tr>
             <tr>
               <th scope="row">2086 – 2100</th>
-              <td>{{ results.gipl_alt_2095 }}</td>
+              <td>{{ results.gipl_alt_2095 }}<UnitWidget unitType="m_in" type="light" /></td>
               <td>
-                {{ signedDiff(results.gipl_alt_1995, results.gipl_alt_2095) }}
+                {{ signedDiff(results.gipl_alt_1995, results.gipl_alt_2095, 'alt') }}<UnitWidget unitType="m_in" type="light" />
               </td>
             </tr>
           </tbody>
@@ -112,7 +119,7 @@
           <li v-if="results.magt_2018">
             Obu et al. (2018) Mean Annual Ground Temperature (deg. C) at Top of
             Permafrost:
-            <strong>{{ results.magt_2018 }}</strong>
+            <strong>{{ results.magt_2018 }}<UnitWidget unitType="temp" type="light" /></strong>
           </li>
           <li v-if="results.giv_2008">
             Jorgenson et al. (2008) Ground Ice Volume:
@@ -152,12 +159,19 @@ export default {
     ...mapGetters({
 			results: "report/results",
       placeName: "report/placeName",
-      latLng: "report/latLng"
+      latLng: "report/latLng",
+      units: "report/units"
     })
   },
   methods: {
-    signedDiff(historical, projected) {
-      let rawDiff = (projected - historical).toFixed(2);
+    signedDiff(historical, projected, variable) {
+      let decimals;
+      if (variable == "alt") {
+        decimals = this.units == "metric" ? 2 : 1;
+      } else if (variable == "magt") {
+        decimals = this.units == "metric" ? 1 : 1;
+      }
+      let rawDiff = (projected - historical).toFixed(decimals);
       if (rawDiff > 0) {
         return "+" + rawDiff;
       } else {
@@ -188,11 +202,15 @@ export default {
 
         let plateResults = {
           place: place,
+
+          // "alt_" substrings are converted between meters and inches
           gipl_alt_1995: this.results["gipl"]["1995"]["cruts31"]["historical"]["alt"],
           gipl_alt_2025: this.results["gipl"]["2025"]["ncarccsm4"]["rcp85"]["alt"],
           gipl_alt_2050: this.results["gipl"]["2050"]["ncarccsm4"]["rcp85"]["alt"],
           gipl_alt_2075: this.results["gipl"]["2075"]["ncarccsm4"]["rcp85"]["alt"],
           gipl_alt_2095: this.results["gipl"]["2095"]["ncarccsm4"]["rcp85"]["alt"],
+
+          // "magt_" substrings are converted between °C and °F
           gipl_magt_1995: this.results["gipl"]["1995"]["cruts31"]["historical"]["magt"],
           gipl_magt_2025: this.results["gipl"]["2025"]["ncarccsm4"]["rcp85"]["magt"],
           gipl_magt_2050: this.results["gipl"]["2050"]["ncarccsm4"]["rcp85"]["magt"],
@@ -206,6 +224,7 @@ export default {
         }
 
         if (this.results["obu_magt"] != null) {
+          // "magt_" substrings are converted between °C and °F
           plateResults["magt_2018"] = this.results["obu_magt"]["temp"];
         }
 
