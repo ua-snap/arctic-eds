0d18396c420f5fd7a67fa48dfdf9453103150348
Bruce Crevensten <bruce.crevensten@gmail.com>
2022-06-16 19:51:34 -0700

    Remixes home page to remove un-implemented sections

--- pages/index.vue
+++ pages/index.vue
@@ -1,115 +1,126 @@
 <template>
   <div>
-    <div class="toc--wrapper">
-      <div class="toc--panel">
-        <NuxtLink to="/physiography">
-          <h3>Physiography</h3>
-          <div class="toc--panel--image image--physiography"></div>
-        </NuxtLink>
-        <ul>
-          <li>Earthquakes</li>
-          <li>Forest Types</li>
-          <li>
-            <NuxtLink to="/physiography/geology">Geology</NuxtLink>
-          </li>
-          <li>Glaciation</li>
-          <li>
-            <NuxtLink to="/physiography/permafrost">Permafrost</NuxtLink>
-          </li>
-          <li>
-            <NuxtLink to="/physiography/physiography"
-              >Physiographic Provinces</NuxtLink
-            >
-          </li>
-        </ul>
+    <div class="toc--wrapper columns mx-5">
+      <div class="blurb column is-one-third mx-3">
+        <h2 class="title is-4 mt-3">
+          Modern environmental data for science and engineering.
+        </h2>
+        <div class="content is-size-5">
+          <p>
+            Technical professionals working on projects in Alaska and the Arctic
+            need readily&ndash;available, high&ndash;quality engineering and
+            environmental data.
+          </p>
+          <p>
+            The Arctic-EDS offers curated, vetted datasets through online maps,
+            processing modules and direct data access.
+          </p>
+          <p>
+            <b-button tag="nuxt-link" to="/about" icon-right="arrow-right">
+              Read more
+            </b-button>
+          </p>
+        </div>
       </div>
-      <div class="toc--panel">
-        <h3>Water</h3>
-        <div class="toc--panel--image image--water"></div>
-        <ul>
-          <li>Coastal Waters</li>
-          <li>Sea Temperatures</li>
-          <li>Sea Ice</li>
-          <li>River and Coastal Ice</li>
-          <li>Tides</li>
-          <li>Water Crop</li>
-        </ul>
-      </div>
-      <div class="toc--panel">
-        <h3>Light</h3>
-        <div class="toc--panel--image image--light"></div>
-        <ul>
-          <li>Sun Path Diagrams</li>
-          <li>Sunlight</li>
-          <li>Twilight</li>
-          <li>Sunlight + Twilight</li>
-        </ul>
-      </div>
-      <div class="toc--panel">
-        <NuxtLink to="/climate">
-          <h3>Climate</h3>
-          <div class="toc--panel--image image--climate"></div>
-        </NuxtLink>
-        <ul>
-          <li>Climatic Zones</li>
-          <li>
-            <NuxtLink to="/climate/precipitation">Precipitation</NuxtLink>
-          </li>
-          <li>Wet Days per Year</li>
-          <li>
-            <NuxtLink to="/climate/snowfall">Snowfall</NuxtLink>
-          </li>
-          <li>
-            <NuxtLink to="/climate/temperature">Temperature</NuxtLink>
-          </li>
-        </ul>
-      </div>
-      <div class="toc--panel">
-        <NuxtLink to="/engineering">
-          <h3>Engineering</h3>
-          <div class="toc--panel--image image--engineering"></div>
-        </NuxtLink>
-        <ul>
-          <li>
-            <NuxtLink to="/engineering/freezing-index">Freezing Index</NuxtLink>
-          </li>
-          <li>
-            <NuxtLink to="/engineering/design-freezing-index"
-              >Design Freezing Index</NuxtLink
-            >
-          </li>
-          <li>
-            <NuxtLink to="/engineering/thawing-index">Thawing Index</NuxtLink>
-          </li>
-          <li>
-            <NuxtLink to="/engineering/design-thawing-index"
-              >Design Thawing Index</NuxtLink
-            >
-          </li>
-          <li>
-            <NuxtLink to="/engineering/heating-degree-days"
-              >Heating Degree Days</NuxtLink
-            >
-          </li>
-          <li>Building Design Criteria</li>
-        </ul>
+      <div class="column">
+        <div class="columns">
+          <div class="column is-one-third">
+            <div class="toc--panel">
+              <NuxtLink to="/physiography">
+                <h3>Physiography</h3>
+                <div class="toc--panel--image image--physiography"></div>
+              </NuxtLink>
+              <ul>
+                <li>
+                  <NuxtLink to="/physiography/geology">Geology</NuxtLink>
+                </li>
+                <li>
+                  <NuxtLink to="/physiography/permafrost">Permafrost</NuxtLink>
+                </li>
+                <li>
+                  <NuxtLink to="/physiography/physiography"
+                    >Physiographic Provinces</NuxtLink
+                  >
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="column is-one-third">
+            <div class="toc--panel">
+              <NuxtLink to="/climate">
+                <h3>Climate</h3>
+                <div class="toc--panel--image image--climate"></div>
+              </NuxtLink>
+              <ul>
+                <li>
+                  <NuxtLink to="/climate/precipitation">Precipitation</NuxtLink>
+                </li>
+                <li>
+                  <NuxtLink to="/climate/snowfall">Snowfall</NuxtLink>
+                </li>
+                <li>
+                  <NuxtLink to="/climate/temperature">Temperature</NuxtLink>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="column is-one-third">
+            <div class="toc--panel">
+              <NuxtLink to="/engineering">
+                <h3>Engineering</h3>
+                <div class="toc--panel--image image--engineering"></div>
+              </NuxtLink>
+              <ul>
+                <li>
+                  <NuxtLink to="/engineering/freezing-index"
+                    >Freezing Index</NuxtLink
+                  >
+                </li>
+                <li>
+                  <NuxtLink to="/engineering/design-freezing-index"
+                    >Design Freezing Index</NuxtLink
+                  >
+                </li>
+                <li>
+                  <NuxtLink to="/engineering/thawing-index"
+                    >Thawing Index</NuxtLink
+                  >
+                </li>
+                <li>
+                  <NuxtLink to="/engineering/design-thawing-index"
+                    >Design Thawing Index</NuxtLink
+                  >
+                </li>
+                <li>
+                  <NuxtLink to="/engineering/heating-degree-days"
+                    >Heating Degree Days</NuxtLink
+                  >
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 <style lang="scss" scoped>
+::v-deep {
+  a.button {
+    color: white !important;
+    background-color: #4b8cc5;
+    border: none;
+    font-weight: 600;
+  }
+}
 .toc--wrapper {
-  display: flex;
-  justify-content: center;
   margin-bottom: 6rem;
 
+  .blurb h2.title {
+    padding-bottom: 0.3rem;
+    border-bottom: 0.3rem solid #4b8cc5;
+  }
   .toc--panel {
-    margin-right: 2vw;
-
-    &:last-of-type {
-      margin-right: 0;
-    }
-
     h3 {
       color: #d43f16;
       font-size: 1.75rem;
@@ -120,12 +131,18 @@
     }
 
     ul {
-      font-size: 1.1rem;
-      margin-top: 1rem;
+      font-size: 1.2rem;
+      margin-top: 0.2rem;
+      li {
+        margin-bottom: 0.25rem;
+        a {
+          color: #d43f16;
+          font-weight: 600;
+        }
+      }
     }
 
     .toc--panel--image {
-      width: 17vw;
       min-height: 12vw;
       background-size: cover;
       transition: background-image 0.2s ease-in-out;
