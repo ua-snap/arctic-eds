a76f915d3f735ec57f0421a081ec0f0b17f6eda1
Craig Stephenson <crstephenson@alaska.edu>
2022-06-20 16:37:19 -0800

    Replace individual years with year ranges in Temperature plate explanation text.

--- components/plates/temperature/Explanation.vue
+++ components/plates/temperature/Explanation.vue
@@ -10,10 +10,11 @@
     </p>
     <p>
       This map shows average annual historical and projected temperatures for
-      1901 and 2099, as well as the coldest, hottest, and average temperatures
-      for those years for the months of January (typically the coldest month)
-      and July (typically the warmest month). It can be seen that the highest
-      temperatures are in the most continental portions of the state.
+      1980&ndash;2009 and 2040&ndash;2069, as well as the coldest, hottest, and
+      average temperatures for those years for the months of January (typically
+      the coldest month) and July (typically the warmest month). It can be seen
+      that the highest temperatures are in the most continental portions of the
+      state.
     </p>
     <p>
       In general, minimum daily temperatures are found at or before sunrise and
