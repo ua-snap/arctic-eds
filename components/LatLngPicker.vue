bc1bb81da732a09a23d894c24028aae2ab18db5c
Craig Stephenson <crstephenson@alaska.edu>
2022-11-04 09:12:08 -0800

    Add support for additional coordinate formats and enter key for lat/lon input (#205)
    * Add support for additional lat/lon formats and enter key.
    
    * Prevent enter key from bypassing lat/lon validation.

--- components/LatLngPicker.vue
+++ components/LatLngPicker.vue
@@ -1,80 +1,66 @@
 <template>
-  <div class="latlng-picker">
-    <div class="field-wrapper">
-      <b-field label="Latitude" horizontal>
-        <b-input v-model="lat" placeholder="64.8436"></b-input>
-      </b-field>
+  <div>
+    <div class="content">
+      <h4 class="title is-5">Find a place by latitude &amp; longitude</h4>
+      <p>
+        Enter a specific lat/lon combo below. Decimal degrees or DMS formats are
+        accepted, i.e. <code>65.24, -142.22</code> or
+        <code>58&deg; 18' 0" N, 134&deg; 24' 57.6" W</code>.
+      </p>
+
+      <div class="columns">
+        <div class="column is-two-thirds">
+          <b-field :type="getFieldStatus" :message="getFieldMessage">
+            <b-input
+              v-model="latlngInput"
+              placeholder="64.8436, -147.7230"
+              @keydown.native.enter="process"
+            ></b-input>
+          </b-field>
+        </div>
+        <div class="column">
+          <b-button :type="type" :disabled="!isValid" @click="process"
+            >Get point data</b-button
+          >
+        </div>
+      </div>
     </div>
-    <div class="field-wrapper">
-      <b-field label="Longtitude" horizontal>
-        <b-input v-model="lng" placeholder="-147.7230"></b-input>
-      </b-field>
-    </div>
-    <b-button v-on:click="processClick" :disabled="!isValid" :type="type"
-      >Get point data</b-button
-    >
   </div>
 </template>
-
-<style lang="scss" scoped>
-.field-wrapper {
-  display: inline-block;
-  ::v-deep input[type='text'] {
-    width: 6rem;
-  }
-  ::v-deep .field-label {
-    margin-right: 1rem;
-  }
-  &:not(:first-child) {
-    margin-left: 2rem;
-    margin-right: 2rem;
-  }
-}
-</style>
 <script>
+import parseDMS from 'parse-dms'
+
 export default {
   name: 'LatLngPicker',
   data() {
     return {
-      lat: undefined,
-      lng: undefined,
-      isValid: false,
       type: 'is-light',
       latLng: undefined,
+      latlngInput: '',
+      fieldMessage: '',
     }
   },
-  watch: {
-    lat() {
-      this.validate()
+  computed: {
+    isValid() {
+      return this.validate()
     },
-    lng() {
-      this.validate()
+    getFieldStatus() {
+      if (this.latlngInput.length < 8 || this.isValid) {
+        return '' // OK
+      } else {
+        return 'is-danger' // not OK
+      }
+    },
+    getFieldMessage() {
+      if (this.latlngInput.length < 8 || this.isValid) {
+        return ''
+      } else {
+        return this.fieldMessage
+      }
     },
   },
   methods: {
-    validate() {
-      let lat = parseFloat(this.lat)
-      let lng = parseFloat(this.lng)
-
-      if (
-        // Check for bbox; these tests will also fail
-        // if the user has entered a non-numeric string.
-        lat >= 51.229 &&
-        lat <= 71.3526 &&
-        lng >= -179.1506 &&
-        lng <= -129.9795
-      ) {
-        // Valid!
-        this.isValid = true
-        this.type = 'is-success'
-        this.latLng = { lat: lat, lng: lng }
-      } else {
-        this.isValid = false
-        this.type = 'is-light'
-        this.latLng = {}
-      }
-    },
-    processClick() {
+    process() {
       if (this.isValid) {
         this.$router.push({
           path:
@@ -86,6 +72,42 @@ export default {
           hash: '#report',
         })
       }
+    },
+    invalidLatLng() {
+      this.fieldMessage = "I can't figure out how to make that a point."
+      this.type = 'is-light'
+      this.latLng = {}
+      return false
+    },
+    validLatLng(lat, lng) {
+      this.fieldMessage = ''
+      this.type = 'is-success'
+      this.latLng = { lat: lat, lng: lng }
+      return true
+    },
+    validate() {
+      if (!this.latlngInput) {
+        return false // do nothing if it's empty
+      }
+
+      try {
+        let parsedDms = parseDMS(this.latlngInput)
+        if (parsedDms && parsedDms.lat && parsedDms.lon) {
+          let lat = parsedDms.lat
+          let lon = parsedDms.lon
+          if (
+            lat >= 51.229 &&
+            lat <= 71.3526 &&
+            lon >= -179.1506 &&
+            lon <= -129.9795
+          ) {
+            return this.validLatLng(lat, lon)
+          }
+        }
+      } catch (e) {
+        return this.invalidLatLng()
+      }
+      return this.invalidLatLng()
     },
   },
 }
