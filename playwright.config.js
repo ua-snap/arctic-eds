ac42585791724ac96c7c00d8ab2d5dbeb4f3092e
Bob Torgerson <rltorgerson@alaska.edu>
2025-08-06 13:33:55 -0800

    Reduce timeout to 60 seconds.

--- playwright.config.js
+++ playwright.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from '@playwright/test'
  * @see https://playwright.dev/docs/test-configuration
  */
 export default defineConfig({
-  timeout: 600000,
+  timeout: 60000,
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'npm run dev',
